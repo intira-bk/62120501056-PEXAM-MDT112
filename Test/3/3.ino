b2ce12b62cd18c8e54ed3bbb9076219694c1b4e1
intira <bluewhalechanal@gmail.com>
2020-07-04 14:11:56 +0700

    3complete

---
+++ Test/3/3.ino
@@ -0,0 +1,22 @@
+const int in1 = 7;
+const int in2 = 6;
+const int ena = 9;
+void setup() 
+{
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(ena, OUTPUT);
+  analogWrite (ena,100);
+  digitalWrite (in1,LOW);
+  digitalWrite (in2,HIGH);
+  Serial.begin(9600);
+  Serial.println("Turn CW");
+  delay(3000);
+  analogWrite (ena,0);
+  digitalWrite (in1,LOW);
+  digitalWrite (in2,HIGH);
+}
+void loop() 
+{
+    
+}
